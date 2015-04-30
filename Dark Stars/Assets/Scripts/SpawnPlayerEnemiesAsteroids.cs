26f26a86522185f5c5615f3a67556025592c34db
wargamer88 <m.van.denzel@home.nl>
2015-04-30 12:53:14 +0200

    added spawn player ship

---
+++ Dark Stars/Assets/Scripts/SpawnPlayerEnemiesAsteroids.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPlayerEnemiesAsteroids : MonoBehaviour {
+
+    //Player
+    public Object spaceShip;
+
+    //list of Enemies
+    public List<Object> spaceShipEnemiesList = new List<Object>();
+
+    //List of Asteroids
+    public List<Object> AsteroidsList = new List<Object>();
+
+	// Use this for initialization
+	void Start () {
+        SpawnPlayerShip();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnPlayerShip()
+    {
+        Instantiate(spaceShip, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
