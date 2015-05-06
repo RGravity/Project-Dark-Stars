b4613f39ea4e22215265d280e01e3b2673994a95
RGravity <rogieroh@hotmail.com>
2015-05-06 10:56:45 +0200

    Euh?

---
+++ Dark Stars/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public GameObject target;
+
+    Vector3 acceleration;
+    Vector3 velocty;
+    float speed = 5.0f;
+
+    GameObject[] boids;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        EnemyMovement();
+	}
+
+    void EnemyMovement()
+    {
+
+    }
+}
