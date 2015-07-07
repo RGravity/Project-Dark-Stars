d3c3c6f75ca8087ce50a5f9e052ad3eae3f8cf8e
wargamer88 <m.van.denzel@home.nl>
2015-07-07 14:34:36 +0200

    fixed goign back with B-button

--- Dark Stars/Assets/Menu Assets/Scripts/LoadOnClick.cs
+++ Dark Stars/Assets/Menu Assets/Scripts/LoadOnClick.cs
@@ -1,23 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LoadOnClick : MonoBehaviour {
+public class LoadOnClick : MonoBehaviour
+{
 
-	public void LoadScene(int Prototype)
-	{
-		Application.LoadLevel (Prototype);
-	}
+    public void LoadScene(int Prototype)
+    {
+        Application.LoadLevel(Prototype);
+    }
 
     public void ExitScene()
     {
         Application.Quit();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.Joystick1Button6))
-        {
-            Application.LoadLevel(0);
-        }
-    }
 }
